ad811e294a44f64ef55f2f85ba23e92745704538
Ji Smith <jihyunjung0219@gmail.com>
2020-08-22 15:08:54 -0500

    Readme update

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,37 @@
 $(document).ready(function () {
     // time set up from moment
-    const now = moment().format('MMMM Do YYYY');
+    const now = moment().format("MMM Do YY");
     $("#currentDay").append(now);
+
+    var currentTime = parseInt(moment().format('H'));
+    // if H === i >>> .present"
+    // if H < i >>> past
+    // if H > i >>> future color
+
+    // var hourNow = currentTime;
+    //     hourNow.addClass("present");
+    // var hourPast = hourPast < currentTime;
+    //     hourPast.addClass("past");
+    // var hourFuture = hourFuture > currentTime;
+    //     hourFuture.addClass("future");
 
 
     // Dynamic Time blocks
     //iterate the hours from 9 to 17
    for (var i=9; i < 18; i++){
 
-    // create div same as this HTML
+    // create div same as this HTML format
 
      /* <div class="row">
-      <div class="hour" id="9"> 9</div>
-      <input class="description"></input>
-      <button class="saveBtn"> save</button>
-    </div> */
+          <div class="hour" id="9"> 9</div>
+          <input class="description"></input>
+          <button class="saveBtn"> save</button>
+       </div> */
 
     var newRow = $("<div></div>");
     // add class name "row"
     newRow.addClass("row");
-    //
+    // make a div for hour
     var hourDiv = $("<div></div>");
     hourDiv.addClass("hour");
     hourDiv.attr("id",i);
@@ -27,18 +39,27 @@ $(document).ready(function () {
 
     var input = $("<input>");
     input.addClass("description");
-    input.addClass("present");
-    // when you refresh
+    if(currentTime === i){
+        input.addClass("present")
+    } else if(currentTime < i ) {
+        input.addClass("past")
+    } else if(currentTime > i) {
+        input.addClass("future")
+    }
+
+    // when you refresh the page it will save the previous data
     input.val(localStorage.getItem(i));
 
     var saveBtn = $("<button>");
     saveBtn.addClass("saveBtn");
     saveBtn.text("SAVE");
 
+    // put them all in the same row
     newRow.append(hourDiv, input, saveBtn);
     $("#name-table").append(newRow);
    }
 
+    // button event
    $(".saveBtn").on("click", function(){
     var userTodo = $(this).siblings("input").val();
     console.log(userTodo);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # 5_Work-Day-Scheduler
+
+you can see the files here :
+> [github] (https://github.com/jiji-smith/5_Work-Day-Scheduler)
+> [published] (https://jiji-smith.github.io/5_Work-Day-Scheduler/)
+
+
+## Key Topics
+
+* JSON
+* AJAX
+* HTTP GET requests
+* Server-side APIs
+
+### 1. What is JSON?
+    JavaScript Object Notation is an open standard file format, and data interchange format, that uses human-readable text to store and transmit data objects consisting of attribute–value pairs and array data types (or any other serializable value). It is a very common data format, with a diverse range of applications, such as serving as a replacement for XML in AJAX systems.[source](https://en.wikipedia.org/wiki/JSON)
+### 2. How does the client-server relationship work?
+    The client-server characteristic describes the relationship of cooperating programs in an application. The server component provides a function or service to one or many clients, which initiate requests for such services. Servers are classified by the services they provide. [source](https://en.wikipedia.org/wiki/Client%E2%80%93server_model#:~:text=The%20client%2Dserver%20characteristic%20describes,by%20the%20services%20they%20provide.)
+### 3. What is an API?
+        An application programming interface (API) is a computing interface which defines interactions between multiple software intermediaries. It defines the kinds of calls or requests that can be made, how to make them, the data formats that should be used, the conventions to follow, etc. It can also provide extension mechanisms so that users can extend existing functionality in various ways and to varying degrees. [source](https://en.wikipedia.org/wiki/API)
