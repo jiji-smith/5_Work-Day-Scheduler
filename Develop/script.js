a304ae0cfab65513bbd7c203adf7b8ac2d7ec3ba
Ji Smith <jihyunjung0219@gmail.com>
2020-08-22 19:47:48 -0500

    add clear btn and updated readme

--- Develop/script.js
+++ Develop/script.js
@@ -1,74 +1,78 @@
 $(document).ready(function () {
     // time set up from moment
-    const now = moment().format("MMM Do YY");
+    const now = moment().format("MMM Do YYYY");
     $("#currentDay").append(now);
 
     var currentTime = parseInt(moment().format('H'));
-    // if H === i >>> .present"
-    // if H < i >>> past
-    // if H > i >>> future color
-
-    // var hourNow = currentTime;
-    //     hourNow.addClass("present");
-    // var hourPast = hourPast < currentTime;
-    //     hourPast.addClass("past");
-    // var hourFuture = hourFuture > currentTime;
-    //     hourFuture.addClass("future");
-
 
     // Dynamic Time blocks
     //iterate the hours from 9 to 17
-   for (var i=9; i < 18; i++){
+    for (var i = 9; i < 18; i++) {
 
-    // create div same as this HTML format
+        // create div same as this HTML format
 
-     /* <div class="row">
-          <div class="hour" id="9"> 9</div>
-          <input class="description"></input>
-          <button class="saveBtn"> save</button>
-       </div> */
+        /* <div class="row">
+             <div class="hour" id="9"> 9</div>
+             <input class="description"></input>
+             <button class="saveBtn"> save</button>
+          </div> */
 
-    var newRow = $("<div></div>");
-    // add class name "row"
-    newRow.addClass("row");
-    // make a div for hour
-    var hourDiv = $("<div></div>");
-    hourDiv.addClass("hour");
-    hourDiv.attr("id",i);
-    hourDiv.text(i + ":00");
+        var newRow = $("<div></div>");
+        // add class name "row"
+        newRow.addClass("row");
+        // make a div for hour
+        var hourDiv = $("<div></div>");
+        hourDiv.addClass("hour");
+        hourDiv.attr("id", i);
+        hourDiv.text(i + ":00");
 
-    var input = $("<input>");
-    input.addClass("description");
-    if(currentTime === i){
-        input.addClass("present")
-    } else if(currentTime < i ) {
-        input.addClass("past")
-    } else if(currentTime > i) {
-        input.addClass("future")
+        // create input field
+
+        var input = $("<input>");
+        input.addClass("description");
+        // change the color by time
+        if (currentTime === i) {
+            input.addClass("present")
+        } else if (currentTime < i) {
+            input.addClass("past")
+        } else if (currentTime > i) {
+            input.addClass("future")
+        }
+
+        // when you refresh the page it will save the previous data
+        input.val(localStorage.getItem(i));
+
+        var saveBtn = $("<button>");
+        saveBtn.addClass("saveBtn");
+        saveBtn.text("SAVE");
+
+        // put them all in the same row
+        newRow.append(hourDiv, input, saveBtn);
+        $("#name-table").append(newRow);
     }
 
-    // when you refresh the page it will save the previous data
-    input.val(localStorage.getItem(i));
 
-    var saveBtn = $("<button>");
-    saveBtn.addClass("saveBtn");
-    saveBtn.text("SAVE");
+    var btnRow = $("<button></button>");
+    // add class name "clearBtn"
+    btnRow.addClass("clearBtn");
+    btnRow.text("CLEAR ALL");
+    $("body").append(btnRow);
 
-    // put them all in the same row
-    newRow.append(hourDiv, input, saveBtn);
-    $("#name-table").append(newRow);
-   }
 
     // button event
-   $(".saveBtn").on("click", function(){
-    var userTodo = $(this).siblings("input").val();
-    console.log(userTodo);
+    $(".saveBtn").on("click", function () {
+        var userTodo = $(this).siblings("input").val();
+        // console.log(userTodo);
 
-    var hourFinder = $(this).siblings(".hour").attr("id");
-    // console.log(hourFinder);
-    localStorage.setItem(hourFinder, userTodo);
+        var hourFinder = $(this).siblings(".hour").attr("id");
+        // console.log(hourFinder);
+        localStorage.setItem(hourFinder, userTodo);
+    });
 
 
-   });
+    $(".clearBtn").on("click", function () {
+        $("input").val('');
+       localStorage.clear();
+    });
 
 });
